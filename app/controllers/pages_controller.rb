718e768dfad8657bd4c887934b9207213f8119ff
Simon Summers <simondsummers@gmail.com>
2016-06-12 17:15:20 +0000

    alphablog master

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  
+  def home
+   
+  end
+  def about
+   
+  end
+  def more
+    
+  end
+  def free
+    
+  end
+  def contact
+    
+  end
+  
+end
